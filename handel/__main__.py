72314e7f3f5ada7982c483e4b61d3ae779b453f0
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2021-09-03 01:54:46 +0530

    bc

--- handel/__main__.py
+++ handel/__main__.py
@@ -10,7 +10,7 @@ Bot = Client(
     Config.API_ID,
     Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="handel.s"),
+    plugins=dict(root="handel.data"),
 )
 if not os.path.isdir("./downloads"):
     os.makedirs("./downloads")
