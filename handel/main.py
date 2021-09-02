3448d36e37780c9751a06d7a00c9719bbd663c9f
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2021-09-03 01:18:56 +0530

    Create main.py

---
+++ handel/main.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import asyncio
+from pyrogram import Client, idle
+from config import Config
+from pyrogram.raw import functions, types
+
+Bot = Client(
+    ":memory:",
+    Config.API_ID,
+    Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
+    plugins=dict(root="bot.data"),
+)
+if not os.path.isdir("./downloads"):
+    os.makedirs("./downloads")
+
+User = Client(
+    Config.SESSION_STRING,
+    Config.API_ID,
+    Config.API_HASH
+)
+
+Bot.start()
+User.start()
+
+idle()
+Bot.stop()
+User.stop()
