a2b576d0ae0669fd077e3a0e684abbc559b5d5ab
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2021-09-03 13:03:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,22 @@ import asyncio
 from pyrogram import Client, idle
 from config import Config
 from pyrogram.raw import functions, types
-
+from handel.user import User
 app = Client(
-    Config.SESSION_STRING,
+    ":memory:",
     Config.API_ID,
     Config.API_HASH,
+    bot_token=Config.BOT_TOKEN,
     plugins=dict(root="handel.data"),
 )
 if not os.path.isdir("./downloads"):
     os.makedirs("./downloads")
 
-async def main(app):
-       await app.start()
-       print('started')
-       await pyrogram.idle()
-       await app.stop()
-       print('stopped')
+
+
+app.start()
+User.start()
+print('Started')
+idle()
+app.stop()
+User.stop()
