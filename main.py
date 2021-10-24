2db88153c3d419981e646e5632e50ccfe9a72d4c
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2021-10-24 17:02:28 +0530

    hh

--- main.py
+++ main.py
@@ -2,24 +2,15 @@ import os
 import sys
 import asyncio
 from pyrogram import Client, idle
-from config import Config
 from pyrogram.raw import functions, types
-from handel.data.user import User
-app = Client(
-    ":memory:",
+
+User = Client(
+    Config.SESSION_STRING,
     Config.API_ID,
-    Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="handel.data"),
+    Config.API_HASH
 )
-if not os.path.isdir("./downloads"):
-    os.makedirs("./downloads")
 
 
 
-app.start()
 User.start()
 print('Started')
-idle()
-app.stop()
-User.stop()
