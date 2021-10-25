14e5a33b5b38741102954c8686a1f6ac9eea0430
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2021-10-25 15:59:44 +0530

    uuuuuuuuuuu

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from pytgcalls import PyTgCalls
 from pytgcalls import StreamType
 from pytgcalls.types import Browsers
 from pytgcalls.types.input_stream import AudioVideoPiped
-from pytgcalls.types.input_stream.quality import HighQualityAudio
-from pytgcalls.types.input_stream.quality import HighQualityVideo
+from pytgcalls.types.input_stream.quality import LowQualityAudio
+from pytgcalls.types.input_stream.quality import LowQualityVideo
 
 from pytgcalls import idle
 S = "BQAx8V0qLC8cDQX0xp6ICAQxglOeHWznUM0mCQm9QKVevKoDXLtC5aynETMtBB-5iv1ZMAlsdqeF-gC1Qbe68XUMlknUKVfGffe4PasUI9DcX2Osf7IgLMIMQ6nXTCF8Tm3F4g_BJ9wmz-85L9g1VPDlEOHvI4ndqk-DRhqPdKSAVBZIyeUGdmxiTdpl6dz_pZYfkwKp0jQTaqQt42l-FD4rYtrjLJBU2Q4fg_-sgx0s-iFQ4lcc53MLl557PCrJAduDcp2wK3reNLkU7SXHvCEec7RxNQVhYc3BB3BX9a3TucbmYyPOtqX_9Hg8rjddxTAJ8zRFrpMUk0yrZ35N9-LqXBjAwgA"
@@ -63,8 +63,8 @@ async def live(app, message: Message):
         message.chat.id,
         AudioVideoPiped(
             remote,
-            HighQualityAudio(),
-            HighQualityVideo(),
+            LowQualityAudio(),
+            LowQualityVideo(),
             headers={
                 'User-Agent': Browsers().chrome_windows,
             },
@@ -128,8 +128,8 @@ async def you(app, message: Message):
           await call_py.join_group_call(
           message.chat.id,
           AudioVideoPiped(viiid,
-            HighQualityAudio(),
-            HighQualityVideo(),
+            LowQualityAudio(),
+            LowQualityVideo(),
           ),
           stream_type=StreamType().pulse_stream,
           )
