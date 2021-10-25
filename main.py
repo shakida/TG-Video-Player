83b1322ae6ff9e37d9f2dbada93a9b6475277e5a
Biswajyoti Ray <78145724+shakida@users.noreply.github.com>
2021-10-25 13:34:52 +0530

    hhhh

--- main.py
+++ main.py
@@ -25,7 +25,25 @@ app.send_message(-1001297289773, f'**💋 Ready to sex!**')
    # print('start')
 
 
-@app.on_message(filters.command(["livx"]) & filters.user(875645659) & filters.group & ~ filters.edited)
+
+def get_youtube_stream():
+    # USE THIS IF YOU WANT ASYNC WAY
+    async def run_async():
+        proc = await asyncio.create_subprocess_exec(
+            'youtube-dl',
+            '-g',
+            '-f',
+            # CHANGE THIS BASED ON WHAT YOU WANT
+            'best[height<=?720][width<=?1280]',
+            '{link}',
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+        stdout, stderr = await proc.communicate()
+        return stdout.decode().split('\n')[0]
+    return asyncio.get_event_loop().run_until_complete(run_async())
+
+@app.on_message(filters.command(["livx"]) & filters.group & ~ filters.edited)
 async def live(app, message: Message):
  try:
     query = ''
@@ -33,7 +51,7 @@ async def live(app, message: Message):
         query += '' + str(i)
   #  print(query)
     remote = query
-    await app.send_message(message.chat.id, f'Trying...')
+    g = await app.send_message(message.chat.id, f'Trying...')
     try:
        await call_py.leave_group_call(message.chat.id)
     except Exception:
@@ -52,6 +70,7 @@ async def live(app, message: Message):
         ),
         stream_type=StreamType().pulse_stream,
         )
+        await g.delete()
         await app.send_message(message.chat.id, f'**STARTING:** `{remote}` in **{message.chat.title}**')
     except Exception as e:
         await app.send_message(message.chat.id, f'ERROR‼️ `{e}`')
@@ -62,6 +81,79 @@ async def live(app, message: Message):
     await app.send_message(message.chat.id, f'ERROR ‼️ `{e}`')
  return
 
+@app.on_message(filters.command(["plv"]) & filters.group & ~ filters.edited)
+async def video(app, message: Message):
+  try:
+     videos = message.reply_to_message
+     if videos.video or videos.document:
+        f = await app.send_message(message.chat.id, f'Downloading..')
+        otp = await app.download_media(videos)
+        try:
+          await call_py.leave_group_call(message.chat.id)
+        except Exception:
+          pass
+        try:
+           call_py.join_group_call(
+           message.chat.id,
+           AudioVideoPiped(
+           otp,
+           ),
+           stream_type=StreamType().pulse_stream,
+           )
+           f.edit(f'**VIDEO STARTED ▶️**')
+        expect Exception as e:
+           await app.send_message(message.chat.id, f'ERROR‼️: `{e}`)
+           pass
+    expect Exception as e:
+           await app.send_message(message.chat.id, f'Video not found!\n `{e}`)
+           return
 
 
+@app.on_message(filters.command(["ytp"]) & filters.group & ~ filters.edited)
+async def you(app, message: Message):
+    try:
+       query = ''
+       for i in message.command[1:]:
+          query += '' + str(i)
+       link = query
+       h = await app.send_message(message.chat.id, f'Trying to play `{link}`')
+       try:
+          await call_py.leave_group_call(message.chat.id)
+       except Exception:
+       # print(e)
+          pass
+       try:
+          cn = await get_youtube_stream()
+          await call_py.join_group_call(
+          message.chat.id,
+          AudioVideoPiped(cn,
+            HighQualityAudio(),
+            HighQualityVideo(),
+          ),
+          stream_type=StreamType().pulse_stream,
+          )
+          h.edit(F'STARTING PLAYING: `{link}` in\n **{message.chat.title}')
+       except Exception as e:
+          await app.send_message(message.chat.id, f'ERROR‼️: `{e}`')
+          pass
+     except Exception as e:
+          await app.send_message(message.chat.id, f'ERROR ‼️: `{e}`')
+          return
+
+@app.on_message(filters.command(["kilx"]) & filters.group & ~ filters.edited)
+async def kill(app, message: Message):
+      try:
+         await call_py.leave_group_call(message.chat.id)
+         await app.send_message(message.chat.id, f'Stoped ⏹️')
+      except Exception as e:
+         return
+
+         
+          
+        
+@call_py.on_stream_end()
+    async def stream_end_handler(client: PyTgCalls, update: Update):
+        await call_py.leave_group_call(update.chat_id)
+        return
+     #  print(f'Stream ended in {update.chat_id}', update)
 idle()
